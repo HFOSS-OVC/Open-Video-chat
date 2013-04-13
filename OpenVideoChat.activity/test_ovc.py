35613df096e0e41051bd2d4381f121776d6ccda0
CCoffie <CalebCoffie@gmail.com>
2013-04-13 19:31:31 -0400

    Changed test script.

--- OpenVideoChat.activity/test_ovc.py
+++ OpenVideoChat.activity/test_ovc.py
@@ -9,14 +9,14 @@ os.system("git pull")
 os.system("sudo python setup.py build")
 os.system("sudo python setup.py dist_xo")
 
-os.system("sugar-install-bundle OpenVideoChat-1.xo")
+os.system("sugar-install-bundle dist/OpenVideoChat-1.xo")
 
-os.system("echo \"---------------\" >> ErrorLog.txt")
-os.system("date >> ErrorLog.txt")
-os.system("echo \"---------------\" >> ErrorLog.txt")
+os.system("echo \"---------------------------------\" >> ErrorLog.txt")
+os.system("zdump EST >> ErrorLog.txt")
+os.system("echo \"---------------------------------\" >> ErrorLog.txt")
 
 
-os.system("sugar-launch org.laptop.OpenVideoChat &>> ErrorLog.txt")
+os.system("sugar-launch org.laptop.OpenVideoChat >> ErrorLog.txt 2>&1")
 
 os.system("git add ErrorLog.txt")
 os.system("git commit -m\"Updating Error Log.\"")
